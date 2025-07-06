d75b511bdd8141d5f70d4585993b4781027dcaad
yashambodekar <144942722+yashambodekar@users.noreply.github.com>
2025-07-06 14:53:01 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/login", {
+      const response = await axios.post("https://artisans-bridge.onrender.com/api/auth/login", {
         email,
         password,
       });
